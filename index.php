ecf502fe652b1f3b1735928cba06f0b44f670f8c
Bryan Nikla <bryannikla@gmail.com>
2017-01-21 18:01:16 -0500

    Moved JS to separate .js file

--- index.php
+++ index.php
@@ -1,6 +1,30 @@
 <?php
 
-echo "This is UR MOM'S gamejam game!";
 
 
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Balls that Shoot Eachother Game!</title>
+    <style>
+        * { padding: 0; margin: 0; }
+        canvas { background: #1a1a1a; display: block; margin: 0 auto; }
+    </style>
+
+
+
+</head>
+<body>
+<br>
+<br>
+<canvas id="gameCanvas" width="1000" height="600"></canvas>
+
+
+
+
+<script type="text/javascript" src="game.js"></script>
+</body>
 
