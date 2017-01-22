2404c59777b2297942959c5e5d6bc445a75be135
Bryan Nikla <bryannikla@gmail.com>
2017-01-21 22:50:39 -0500

    Lots of work on actual webpage. ------- logos added as background images aside the actual game. navbar added with links to instructions section and about section
    instructions section beginning to be filled in, controls listed and formatted

--- index.php
+++ index.php
@@ -1,30 +1,92 @@
 <?php
 
-
-
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
+    <title>GGJ 2017 Game!</title>
     <meta charset="utf-8" />
-    <title>Balls that Shoot Eachother Game!</title>
-    <style>
-        * { padding: 0; margin: 0; }
-        canvas { background: #1a1a1a; display: block; margin: 0 auto; }
-    </style>
-
-
-
+    <link rel="stylesheet" type="text/css" href="main.css">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu:700" rel="stylesheet">
 </head>
-<body>
-<br>
-<br>
-<canvas id="gameCanvas" width="1000" height="600"></canvas>
+<body id="top">
+<nav>
+    <h1>Global Game Jam 2017 - Balls that Shoot Each Other Game</h1>
+    <div class="navLink">
+        <a href="#instructions">
+            Instructions
+        </a>
+    </div>
+    <div class="navLink">
+        <a href="#about">
+            About
+        </a>
+    </div>
+</nav>
+<div id="logoWrapper">
+    <div id="leftLogo"></div>
+    <div id="rightLogo"></div>
+</div>
+<!-- GAME CANVAS -->
+<a href="#top">
+    <div id="backToTop">
+        <img src="up.png">
+    </div>
+</a>
+<div id="canvasWrapper">
+    <canvas id="gameCanvas" width="1000" height="600"></canvas>
+</div>
+<div id="spacer" style="width: 100%; height: 610px;">&nbsp;</div>
+<section id="instructions">
+    <h2>Game Instructions</h2>
+    <div id="controlsContainer">
+        <div id="player1">
+            <h3>Player 1</h3>
+            <hr>
+            <h4>
+                <div class="control">Fire Up:</div>
+                <div class="key">&nbsp;&nbsp;W</div>
+            </h4>
+            <h4>
+                <div class="control">Fire Down:</div>
+                <div class="key">&nbsp;&nbsp;S</div>
+            </h4><h4>
+                <div class="control">Fire Left:</div>
+                <div class="key">&nbsp;&nbsp;A</div>
+            </h4><h4>
+                <div class="control">Fire Right:</div>
+                <div class="key">&nbsp;&nbsp;D</div>
+            </h4>
+        </div>
+        <div id="player2">
+            <h3>Player 2</h3>
+            <hr>
+            <h4>
+                <div class="control">Fire Up:</div>
+                <div class="key">&nbsp;&nbsp;Up-Arrow</div>
+            </h4>
+            <h4>
+                <div class="control">Fire Down:</div>
+                <div class="key">&nbsp;&nbsp;Down-Arrow</div>
+            </h4><h4>
+                <div class="control">Fire Left:</div>
+                <div class="key">&nbsp;&nbsp;Left-Arrow</div>
+            </h4><h4>
+                <div class="control">Fire Right:</div>
+                <div class="key">&nbsp;&nbsp;Right-Arrow</div>
+            </h4>
+        </div>
+    </div>
+
+</section>
+<section id="about">
+    <h2>About This Project</h2>
+</section>
 
 
 
-
+<!-- GAME SCRIPT -->
 <script type="text/javascript" src="game.js"></script>
 </body>
 
